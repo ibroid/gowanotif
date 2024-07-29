ec266c96d7c80dfd55c84577aa5e5e1a68117352
ibroid <maulana858@gmail.com>
2024-07-29 11:25:31 +0700

    adding waha thidr party

--- whatsapp/waha_third_party.go
+++ whatsapp/waha_third_party.go
@@ -15,14 +15,10 @@ func SendMessageViaWAHA(session string, number string, message string) {
 		"session": session,
 	})
 
-	statusCode, body, errs := ua.Bytes()
+	statusCode, _, errs := ua.Bytes()
 	if len(errs) > 0 {
 		log.Log.Error("Gagal send message via WA HA:", errs)
 	}
 
-	if statusCode != 200 {
-		log.Log.Error("Gagal send message via WA HA, status code:", statusCode)
-	}
-
-	log.Log.Info("Message sent via WA HA . Response :", string(body))
+	log.Log.Info("Message sent via WA HA . Response :", statusCode)
 }
